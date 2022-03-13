d6770a45301208ec7c8011574e6f6ab1613d32da
SaraLucasH <s.lucas.2016@alumnos.urjc.es>
2022-03-13 20:21:42 +0100

    SLH Alg 2 | Miller Rabin

---
+++ algorithm_2.py
@@ -0,0 +1,29 @@
+class algorithm_2_output:
+    a_collection=[]
+    i=0
+
+#Input: T, s
+#Returns:
+#   A collection of values returned by Dodis function. 
+#   First index inside the previous collection that is supposed to be prime
+def algorithm_2(T:int,s:int) -> algorithm_2_output:
+    ctr,i,j=0
+    result:algorithm_2_output = algorithm_2_output()
+    while (ctr<2 and j<T):
+        j+=1
+        aj = dodis_yampolsky(s,j)
+        result.a_collection.insert(aj)
+
+        if miller_rabin(b, e, aj):
+            if(ctr==0):
+                result.i=j
+            ctr+=1  
+
+    #Not valid
+    if ctr<2:
+        result.i=-1
+        return result
+    
+    return result
+   
+
---
+++ miller_rabin-primetest.py
@@ -0,0 +1,47 @@
+from math import log2
+from random import randint, random
+
+
+def miller_rabin(n:int) -> bool :
+    #Step 1
+    prev_n= n-1
+    
+    s = round(log2(prev_n))
+    if s==1 :
+        return False
+    d = prev_n/s 
+
+    # n - 1 must be even
+    if(prev_n % 2 >0):
+        return False
+
+    #Step 2. (seed() function has not be called, so random function's seed is by default 1970)
+    a = randint(2, n-2)   
+
+    #Step 3.
+    x= a^d % n
+    aux = x % n
+    if(aux == 1  or aux == -1):
+        return True
+
+    r=1
+    #Step 4
+    while True:
+        x = a^((2^r)*d) % n
+        if(x==1):
+            return False
+        elif(x==-1):
+            return True
+        
+        r+=1
+        if(r < s-1):
+            break
+
+    #Step 5: r == s-1
+    x = a^((2^(s-1))*d) % n 
+    if(x==-1):
+        return True
+    return False
+
+
+    
